baf62b191997eb4db6ee44fe9596d0cb46bda536
had-esf <adrien.velleret@gmail.com>
2024-10-16 14:54:55 +0200

    tentative désepérer de faire marcher le serveur

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-docker kill app
-docker rm app
-
-docker run -dp 3000:3000 \
-    -ti \
-    --name app \
-    -w /app --mount type=bind,src="$(pwd)",target=/app \
-    node:18-alpine \
-    sh -c "npm install && npm run dev"
