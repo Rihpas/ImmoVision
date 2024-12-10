7f64a198c452dd86a4a14ba19820944f04fcc633
UlyssePoirot <ulysse.poirot81@gmail.com>
2024-12-10 14:47:15 +0100

    met en place Apoollo et Mongo

---
+++ server/app.js
@@ -0,0 +1,88 @@
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const mongoUrl = 'mongodb://localhost:27017';
+// Définition du schéma
+const typeDefs = `
+  type Client {
+    _id : ID!
+    name : String!
+  }
+ 
+  type Query {
+    clients: [Client]
+    client(_id: ID!): Client
+  }
+ 
+  
+`;
+async function getData() {
+    
+    try {
+        const client = new MongoClient(mongoUrl);
+        await client.connect();
+     
+        // Sélectionne la base de données et la collection 
+        const collection = client.db('personne').collection('personnes');
+     
+        // Rechercher une nouvelle donnée dans la collection
+        const res = await collection.find().toArray();
+     
+        // Fermer la connexion
+        await client.close();
+        console.log(res);
+        return res;
+      
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
+// Données simulées
+
+// Resolvers : Fournissent la logique pour les requêtes et mutations
+const resolvers = {
+  Query: {
+    clients: () => getData(),
+    client: (_, { _id }) => clients.find(client => client._id === _id),
+  },
+  
+  };
+
+ 
+// Création du serveur Apollo
+const server = new ApolloServer({ typeDefs, resolvers });
+
+startStandaloneServer(server, {
+    listen: { port: 4000 },
+  }).then(({ url }) => {
+    console.log(`🚀 Serveur prêt à l'adresse : ${url}`);
+  });
+ 
+ 
+ 
+  function sendData() {
+    const dataInput = document.getElementById('dataInput');
+    const outputDiv = document.getElementById('output');
+   
+    const data = dataInput.value;
+   
+    // Utilisez fetch pour envoyer des données au serveur
+    fetch('/sendData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data }),
+    })
+    .then(response => response.json())
+    .then(result => {
+      outputDiv.innerText = `Server Response: ${result.message}`;
+    })
+    .catch(error => {
+      console.error('Error sending data:', error);
+    });
+  }
+
+const { MongoClient } = require('mongodb');
+
+ 
