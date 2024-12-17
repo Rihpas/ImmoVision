06b68271510f7327c47939eadeefe078da40d3a1
had-esf <adrien.velleret@gmail.com>
2024-12-17 20:03:46 +0100

    Restructuration globale

--- server/Index.ts
+++ server/Index.ts
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.status(200).send("requête réussi");
+
+  if 
 });
 
 app.listen(port, () => {
