57ae3ff82ac70cefb3b53c2affed0cc06ac11706
had-esf <adrien.velleret@gmail.com>
2024-12-18 11:29:27 +0100

    status 404

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/login',routes);
 //A vérifier si le doissier routes ne creer pas de conflit entre authentification et database
 app.get('/database',routes);
 
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
 app.listen(port, () => {
   console.log(`Application exemple à l'écoute sur le port ${port}!`);
 });
